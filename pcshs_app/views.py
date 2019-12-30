cf8591e1db2e6b4eb0b0e4ce6d504d4880b24ae7
Abhishek-Kathayat <abhishekkathayat25@gmail.com>
2019-12-30 11:13:19 +0530

    Backend setup

--- pcshs_app/views.py
+++ pcshs_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
 # Create your views here.
+def processHeartData(request):
+    hrtdata = request.body.decode('utf-8')
+    heart_data = json.load(hrtdata)
+    for key, value in heart_data.items():
+        print(key, ':', value)
--- pcshs_backend/settings.py
+++ pcshs_backend/settings.py
@@ -26,11 +26,16 @@ SECRET_KEY = '9-yauq_9p1%asbm087lxy$dq4g2xob3$s78pc10xd#oftum8vs'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080"
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pcshs_app.apps.PcshsAppConfig'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
