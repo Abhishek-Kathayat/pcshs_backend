76465baa15ae8e3448b5e81d705d2f94c8c1f8d9
Abhishek-Kathayat <abhishekkathayat25@gmail.com>
2020-01-02 18:24:41 +0530

    JSON Data handling

--- pcshs_app/views.py
+++ pcshs_app/views.py
@@ -8,6 +8,59 @@ import json
 def processHeartData(request):
     hrtdata = request.body.decode('utf-8')
     heart_data = json.loads(hrtdata)
-    for key, value in heart_data.items():
-        print(key, ':', value)
+
+    # Process Data.
+    hrt_data = list()
+    for key, value in heart_data['body'].items():
+        if(key == 'Gender_js'):
+            if value == 'Male':
+                hrt_data.append(1)
+            else:
+                hrt_data.append(0)
+        elif(key == 'chptype_js'):
+            if value == 'Typical Angina':
+                hrt_data.append(1)
+            elif value == 'Atypical Angina':
+                hrt_data.append(2)
+            elif value == 'Non-Anginal Pain':
+                hrt_data.append(3)
+            else:
+                hrt_data.append(4)
+        elif(key == 'bsl_js'):
+            if value == 'True':
+                hrt_data.append(1)
+            else:
+                hrt_data.append(0)
+        elif(key == 'rer_js'):
+            if value == 'Normal':
+                hrt_data.append(0)
+            elif value == 'Having ST-T wave abnormality':
+                hrt_data.append(1)
+            else:
+                hrt_data.append(2)
+        elif(key == 'eia_js'):
+            if value == 'Yes':
+                hrt_data.append(1)
+            else:
+                hrt_data.append(0)
+        elif(key == 'slpst_js'):
+            if value == 'Upsloping':
+                hrt_data.append(0)
+            elif value == 'Flat':
+                hrt_data.append(1)
+            else:
+                hrt_data.append(2)
+        elif(key == 'thal_js'):
+            if value == 'Normal':
+                hrt_data.append(1)
+            elif value == 'Fixed Defect':
+                hrt_data.append(2)
+            else:
+                hrt_data.append(3)
+        elif(key == 'stdep_js'):
+            hrt_data.append(float(value))
+        else:
+            hrt_data.append(int(value))
+
+    print(hrt_data)
     return HttpResponse('OK')
--- pcshs_backend/settings.py
+++ pcshs_backend/settings.py
@@ -31,13 +31,6 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:8080",
     "http://127.0.0.1:8080"
 ]
-#CORS_ALLOW_HEADERS = list(default_headers) + [
-#    'X_CSRFTOKEN',
-#    'CSRF_COOKIE'
-#]
-#
-#CORS_ALLOW_CREDENTIALS = True
-#CSRF_HEADER_NAME = 'X_CSRFTOKEN'
 
 # Application definition
 
